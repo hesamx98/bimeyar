02266db13075bf16de65a6c2d4b39a1438d3dca3
hesam <hesamx98@gmail.com>
2019-07-24 20:10:04 +0430

    V7

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 function open_part(show) {
 
   if (show === "add"){
-    document.getElementById("add").hidden = true;
-    document.getElementById("show").hidden = false;
+    document.getElementById("add").hidden = false;
+    document.getElementById("show").hidden = true;
   }
 
   if (show === "show"){
-    document.getElementById("add").hidden = false;
-    document.getElementById("show").hidden = true;
+    document.getElementById("add").hidden = true;
+    document.getElementById("show").hidden = false;
   }
 }
 
 function close_part() {
     document.getElementById("show").hidden = true;
-    document.getElementById("add").hidden = true;
 }
