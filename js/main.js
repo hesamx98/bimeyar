9e6cc8edee82c1ac229dbda2461cf20cb434dcfd
hesam <hesamx98@gmail.com>
2019-07-24 23:11:10 +0430

    V9

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ function open_part(show) {
 function close_part() {
     document.getElementById("show").hidden = true;
 }
+
+function filter_table() {
+  var td_num = document.getElementById("td_num").value;
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("filter_input");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("panel_table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[td_num];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
