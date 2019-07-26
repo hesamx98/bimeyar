9885c481246220e7233e26f392935849185789f7
hesam <hesamx98@gmail.com>
2019-07-26 11:58:55 +0430

    V 1.2

--- php/lib/database.php
+++ php/lib/database.php
@@ -1,7 +1,45 @@
 <?php
-
-
 class database
 {
 
+
+  public function connect(){
+    try
+    {
+      $host = "localhost";
+      $dbname = "bimeyar";
+      $username = "root";
+      $password = "";
+
+      $connection = new PDO("mysql:host=$host;dbname=$dbname", "$username", "$password");
+      $connection->exec('set names utf8');
+
+      return $connection;
+    }
+
+    catch
+    (PDOException $e){
+      echo $e . getMessage();
+      return($e . errorInfo());
+    }
+  }
+
+  public function insert($sql,$connection){
+    try {
+
+      $stmt = $connection->query($sql);
+
+      if($stmt == true)
+        echo "true";
+
+      else
+        echo "false";
+
+    } catch
+    (PDOException $e) {
+      echo $e . getMessage();
+      print_r($e . errorInfo());
+    }
+  }
 }
+
--- php/login.php
+++ php/login.php
@@ -1,1 +1,2 @@
 <?php
+ include_once ("lib/DB_class.php");
